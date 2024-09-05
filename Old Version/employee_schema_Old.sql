121c472d58382cf9cb19dbfa5a03437ce8190e46
Zara <zara.pazouki@gmail.com>
2024-09-04 23:28:16 -0400

    New schema Version

--- Old Version/employee_schema_Old.sql
+++ Old Version/employee_schema_Old.sql
@@ -1,22 +1,22 @@
 CREATE TABLE titles(
-	title_id VARCHAR(10) PRIMARY KEY,
-	title VARCHAR(30) NOT NULL
+	title_id VARCHAR PRIMARY KEY,
+	title VARCHAR NOT NULL
 );
 
 
 CREATE TABLE departments(
-	dept_no VARCHAR(10) PRIMARY KEY,
-	dept_name VARCHAR(50)
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR
 );
 
 
 CREATE TABLE employees(
 	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR(10) NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
 	birth_date VARCHAR NOT NULL,
-	first_name VARCHAR(30) NOT NULL,
-	last_name VARCHAR(30) NOT NULL,
-	sex VARCHAR(1) NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
 	hire_date VARCHAR NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
@@ -25,7 +25,7 @@ CREATE TABLE employees(
 -- The dept_emp table is many-many relationship, so two primary keys are needed
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no VARCHAR(10) NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
@@ -41,7 +41,7 @@ CREATE TABLE salaries(
 
 -- The dept_manager table is many-many relationship, so two primary keys are needed
 CREATE TABLE dept_manager(
-	dept_no VARCHAR(10) NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
 	PRIMARY KEY (dept_no, emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
